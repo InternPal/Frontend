b4d5417d506a8faaaca08470c3d23654caeefd57
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-10-11 21:27:29 +0530

    css stylesheets added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import BasicInfoForm from "components/BasicInfoForm";
+
 const App = ()=>{
-  return <div>
-    <h1>InternPal</h1>
-  </div>;
+    return <div>
+        <h1>InternPal</h1>
+        <BasicInfoForm />
+    </div>
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Redirect, Route} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.3.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import App from "./App";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path = "/"><App/></Route>
+      <Redirect to="/"/>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
